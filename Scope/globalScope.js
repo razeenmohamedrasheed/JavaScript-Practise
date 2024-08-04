71236108d7b23993d4cdafb6375c08bf931340d0
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-08-04 20:50:32 +0530

    chnages

---
+++ Scope/globalScope.js
@@ -0,0 +1,12 @@
+var name1 = "razeen"
+var name1 = "updated"
+name1 = "final update"
+
+
+if (true){
+    let name1 = "em,pty"
+    console.log(name1)
+}
+// let name1 = "razeen" error
+// const name1 = "razeen" error
+console.log(name1)
--- functions/functions.js
+++ functions/functions.js
@@ -13,7 +13,22 @@ const expressionFunction = function(){
 const expressionFunctionParam = function(param){
     console.log(`function called using expression..... also with ${param}`)
 }
+
+function x(){
+    console.log("C value....")
+    return "Data"
+}
+let print = x()
+
 basic()
 basicParams("parameter")
 expressionFunction()
 expressionFunctionParam("new parameter")
+x()
+console.log(print)
+
+function test(){
+    var abc ="testtt"
+}
+
+console.log(abc)
