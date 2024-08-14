a4e04099fee4a3327cb23ca7e43406d3d5ce3404
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-08-14 21:30:08 +0530

    Promises full notes
    .all
    .allSettled
    .any
    .race

--- Promises3/Promises3.js
+++ Promises3/Promises3.js
@@ -1,3 +1,6 @@
+//Basics of promise using reolve and reject 
+
+
 let sample = new Promise((resolve,reject)=>{
     let data = "test"
     if(data=="test"){
@@ -8,6 +11,33 @@ let sample = new Promise((resolve,reject)=>{
 })
 
 sample.then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log(err)
+})
+
+const promise1 = new Promise((resolve,reject)=>{
+    resolve(["java","python","GoLang"])
+})
+
+const promise2 = new Promise((resolve,reject)=>{
+    // resolve(["React","Angular","Vue"])
+    reject("failed")
+})
+
+
+const promiseData = Promise.all([promise1,promise2])
+const promiseData1 = Promise.allSettled([promise1,promise2])
+const promiseData3 = Promise.any([promise1,promise2])
+
+
+promiseData.then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log(err)
+})
+
+promiseData1.then((res)=>{
     console.log(res)
 }).catch((err)=>{
     console.log(err)
