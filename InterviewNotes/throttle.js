eb0d1c897b9680e422b4efa21a9ac7c39f7b1023
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-08-26 23:21:36 +0530

    Create throttle.js

---
+++ InterviewNotes/throttle.js
@@ -0,0 +1,24 @@
+// Throttle function: will limit the number of times a function can be called over time
+function throttle(func, delay) {
+    let lastCall = 0;
+    
+    return function(...args) {
+      const now = new Date().getTime();
+      
+      if (now - lastCall < delay) {
+        return;
+      }
+      
+      lastCall = now;
+      return func(...args);
+    };
+  }
+  
+  // Example usage
+  function onResize() {
+    console.log('Window resized:', new Date().toLocaleTimeString());
+  }
+  
+  // Throttled resize event listener
+  window.addEventListener('resize', throttle(onResize, 2000));
+  
